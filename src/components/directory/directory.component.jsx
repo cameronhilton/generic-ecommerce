5066c18f561aa71dae62983f8fcaba0034a4ae45
Cameron Hilton <cameronhilton@gmail.com>
2020-09-19 18:54:21 -0700

    add large image size option to menu items

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -16,8 +16,8 @@ class Directory extends Component {
     return (
       <div className='directory-menu'>
         {
-          this.state.sections.map(({ id, imageUrl, title }) => (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} />
+          this.state.sections.map(({ id, imageUrl, size, title }) => (
+            <MenuItem key={id} size={size} title={title} imageUrl={imageUrl} />
           ))
         }
       </div>
