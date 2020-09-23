353b2aa35eeaea6caa81490d44e347727e2046e5
Cameron Hilton <cameronhilton@gmail.com>
2020-09-23 15:03:07 -0700

    allow multiple quantities of items in cart

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, imageUrl, name, price }) => (
- <div className='collection-item'>
-  <div
-    className='image'
-    style={{
-      backgroundImage: `url(${imageUrl})`,
-    }}
-  />
-  <div className='collection-footer'>
-    <span className='name'>{name}</span>
-    <span className='price'>{price}</span>
-  </div>
-  <CustomButton inverted>Add to cart</CustomButton>
- </div>
-);
+const CollectionItem = ({ addItem, item }) => {
+  const {imageUrl, name, price} = item;
 
-export default CollectionItem;
+  return (
+    <div className='collection-item'>
+    <div
+      className='image'
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className='collection-footer'>
+      <span className='name'>{name}</span>
+      <span className='price'>{price}</span>
+    </div>
+    <CustomButton inverted onClick={() => addItem(item)}>Add to cart</CustomButton>
+   </div>
+  );
+};
+
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -9,8 +9,8 @@ const CollectionPreview = ({ items, title }) => (
       {
         items
           .slice(0, 4)
-          .map(({ id, ...otherItemProps }) => (
-            <CollectionItem key={id} {...otherItemProps} />
+          .map(item => (
+            <CollectionItem key={item.id} item={item} />
           ))
       }
     </div>
