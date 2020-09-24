1fd23d18ae62193258dd33d1c5fe18f92fd4418b
Cameron Hilton <cameronhilton@gmail.com>
2020-09-24 14:12:25 -0700

    add rows for each checkout item in cart

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import '../../components/checkout-item/checkout-item.component';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 import './checkout.styles.scss';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className='checkout-page'>
@@ -24,7 +26,9 @@ const CheckoutPage = ({ cartItems, total }) => (
       </div>
     </div>
     {
-      cartItems.map(cartItem => cartItem.name)
+      cartItems.map(cartItem => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))
     }
     <div className='total'>
       <span>Total: ${total}</span>
