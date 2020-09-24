869a27f109f6bd57b90b5ffcb82e830019f016d5
Cameron Hilton <cameronhilton@gmail.com>
2020-09-24 13:32:57 -0700

    convert remaining components to use structured selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { createStructuredSelector } from 'reselect';
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
@@ -12,8 +13,8 @@ const CartIcon = ({ toggleCartHidden }) => (
   </div>
 );
 
-const mapStateToProps = state => ({
-  itemCont: selectCartItemsCount(state),
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount,
 });
 
 const mapDispatchToProps = dispatch => ({
