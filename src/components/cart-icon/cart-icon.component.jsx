e08cacb2df26905feaa4239d855a82b419fddd79
Cameron Hilton <cameronhilton@gmail.com>
2020-09-24 13:13:59 -0700

    use reselect to prevent unneeded rerenders

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
@@ -11,8 +12,12 @@ const CartIcon = ({ toggleCartHidden }) => (
   </div>
 );
 
+const mapStateToProps = state => ({
+  itemCont: selectCartItemsCount(state),
+});
+
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
