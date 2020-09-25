10592c62aa539a704292367d4855ab3b530e1db0
Cameron Hilton <cameronhilton@gmail.com>
2020-09-25 13:59:47 -0700

    fix render of directory component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -5,10 +5,10 @@ import { createStructuredSelector } from 'reselect';
 import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 import MenuItem from '../menu-item/menu-item.component';
 
-const Directory =({ sections }) => (
+const Directory = ({ sections }) => (
   <div className='directory-menu'>
     {
-      this.state.sections.map(({ id, ...otherSectionProps }) => (
+      sections.map(({ id, ...otherSectionProps }) => (
         <MenuItem key={id} {...otherSectionProps} />
       ))
     }
