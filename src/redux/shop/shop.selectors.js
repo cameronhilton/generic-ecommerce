38d94a6d2a741d843d586da109d7d22b54ddf241
Cameron Hilton <cameronhilton@gmail.com>
2020-09-25 14:15:10 -0700

    create selectors and components with redux flow in directory/shop

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,23 @@
 import { createSelector } from 'reselect'
+import memoize from 'lodash.memoize';
 
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
   [selectShop],
   shop => shop.collections,
+);
+
+// Convert collections object to array of values
+export const selectColelctionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+)
+
+export const selectCollection = memoize(
+  collectionUrlParam =>
+    createSelector(
+      [selectCollections],
+      collections => collections[collectionUrlParam]
+    )
 );
