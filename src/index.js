3998886ac33a1ea99876d7d0ca8dc596d3acab8a
Cameron Hilton <cameronhilton@gmail.com>
2020-11-21 17:50:21 -0800

    add service worker for pwa

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store";
+import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 import App from "./App";
 
@@ -19,3 +20,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+serviceWorker.register();
